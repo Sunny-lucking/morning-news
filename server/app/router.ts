f775dde107b94f6e80846febee6be675b9d36a9d
yangmupei <1419-yangmupei@users.noreply.git.sysop.bigo.sg>
2022-08-05 17:38:17 +0800

    fix: 删除没用代码

--- server/app/router.ts
+++ server/app/router.ts
@@ -3,8 +3,6 @@ import { Application } from 'egg';
 export default (app: Application) => {
   const { controller, router } = app;
 
-  router.get('/', controller.home.index);
-
   // 早报爬虫
   router.get('/morningPaper', controller.morningPaper.index);
   // 推送微信机器人消息
